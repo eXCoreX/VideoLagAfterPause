04f6c9c79185951241e6aa652c06cc9ea4288a80
Rostyslav Litvinov <rostislav.litvinov@ekreative.com>
2023-10-23 21:44:27 +0300

    Initial Commit

---
+++ VideoLagAfterPause/VideoLagAfterPauseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoLagAfterPauseApp.swift
+//  VideoLagAfterPause
+//
+//  Created by Rostyslav Litvinov on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoLagAfterPauseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
